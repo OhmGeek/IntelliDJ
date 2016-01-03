0bba2d850b8abdcd127bdd4cc6021f4fb7d03234
Ryan Collins <ryan_snowball@yahoo.co.uk>
2016-01-03 21:44:24 +0000

    Added the class structure to the code
    By creating an instance of the Spotify Loader class, the get_playlist_track_list method can be called to list all of the track IDs of the songs in the playlist. This class just obtains the track IDs from various methods, and it will also later be used to interface with Spotify easily.

--- spotifySongLoader.py
+++ spotifySongLoader.py
@@ -1,9 +1,16 @@
 import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-cID = "clientID"
-cSecret = "secretID"
-credManager = SpotifyClientCredentials(client_id = cID,client_secret =cSecret)
-spotify = spotipy.Spotify(client_credentials_manager = credManager)
-l = spotify.user_playlist_tracks("USER ID",playlist_id="PLID", fields = 'items(track(id))',limit = 100)
-print l
+class SpotifyLoader(object):
+	"""docstring for SpotifyLoader"""
+	def __init__(self, clientID,clientSecret):
+		cred_manager = SpotifyClientCredentials(client_id = clientID, client_secret = clientSecret)
+		self.spotify = spotipy.Spotify(client_credentials_manager = cred_manager)
+
+	def get_playlist_track_list(self, userID, playlistID,lim = 100):
+		data = self.spotify.user_playlist_tracks(userID, playlist_id = playlistID, fields = 'items(track(id))',limit = lim)
+		data = data['items'] #get rid of other items. Store as a list
+		return_data = []
+		for item in data:
+			return_data.append(item['track']['id'])
+		return return_data
