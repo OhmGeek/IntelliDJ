efae433e802c7d831e7cfe010c8b7c106783af9f
Ryan Collins <ryan_snowball@yahoo.co.uk>
2016-01-06 20:49:09 +0000

    Upgrades to natural selection/representation
    Lots done here!
    
    Firstly, the population is now sorted in order of fitness - the mix with the lowest value for the fitness function is stored at the front of the list, and the whole list is in ascending order of fitness function values. The best solution can therefore be found by finding the first item in this list. When adding new children to the list, they are added in the correct location by finding the location that they should be inserted at (using linear search, so this might be able to be improved with some clever binary search type algorithm, but maybe not).
    
    Secondly, natural selection now works by selecting two random parents from the population (that are in the top levels of fitness). This adds a bit more randomness to the whole system.
    
    TODO: Population culling - occasionally killing off the bad mixes; more children produced per generation (call the breed function a random number of times per generation).

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -1,14 +1,78 @@
+import random
+import math
+from trackFetcher import TrackFetcher
+from spotifySongLoader import SpotifyLoader
+
 class GeneticAlgorithm(object):
 	"""docstring for GeneticAlgorithm"""
 	def __init__(self, initialSize,songList,ENAPIKey):
 		self.population = []
 		self.getDataForSongs(songList,ENAPIKey)
 		self.generatePopulation(songList,initialSize)
-
+		self.SortPopulation()
 		#tests
 		# print(self.population)
 		# print(self.songInfo)
+	
+	def Merge(self,l,r,lf,rf):
+		result = []
+		printOut = False
+		if(l == [3,7,7,26]):
+			printOut = True
+
+		while(len(l) > 0 or len(r) > 0):
+			if(len(l) > 0 and len(r) > 0):
+				if(lf[0] <= rf[0]):
+
+					result.append(l.pop(0))
+					lf.pop(0)
+				else:
+					result.append(r.pop(0))
+					rf.pop(0)
+			elif(len(l) > 0):
+				result.extend(l)
+				l = []
+				lf = []
+			else:
+				result.extend(r)
+				r = []
+				rf = []
+		return result
+
+	def MergeSort(self,li,fitness):
+		if(len(li) <= 1):
+			return li
+
+		mid = (len(li) + 1) / 2
 		
+		l = li[0:mid]
+
+
+		lf = fitness[0:mid]
+
+		l = self.MergeSort(l,lf)
+		lf = self.MergeSort(lf,lf)
+
+
+		r = li[mid:]
+
+		rf = fitness[mid:]
+
+		r = self.MergeSort(r,rf)
+		rf = self.MergeSort(rf,rf)
+
+		result = self.Merge(l,r,lf,rf)
+		return result
+
+	def SortPopulation(self):
+		self.fitness = []
+		for a in self.population:
+			f = self.fitnessFunction(a)
+			self.fitness.append(f)
+		self.population = self.MergeSort(self.population,self.fitness)
+		self.fitness = self.MergeSort(self.fitness,self.fitness)
+
+
 	def generatePopulation(self,songs,initialSize):
 		if(len(songs) == 0):
 			print("No Songs!")
@@ -34,42 +98,62 @@ class GeneticAlgorithm(object):
 			print("Fitness: " + str(f))
 			fitness.append(f)
 		print("")
-		bestPairIndices = self.getTwoMinFitnessIndices(fitness)
-		self.bestIndex = bestPairIndices[0]
-		print("Best indices at: " + str(bestPairIndices[0]) + " and " + str(bestPairIndices[1]))
-		worstPairIndices = self.getTwoMaxFitnessIndices(fitness)
-		self.breedPair(bestPairIndices,worstPairIndices)
-		print("Best solution so far: " + str(self.population[bestPairIndices[0]]))
 
-	def breedPair(self,parentIndices,destinationIndices):
+		b1Index = 0
+		b2Index = 0
+
+		while(b1Index == b2Index):
+			b1Index = random.randint(0,(len(self.population) - 1)/2)
+			b2Index = random.randint(0,(len(self.population)-1)/2)
+
+		bestPairIndices = []
+		bestPairIndices.append(b1Index)
+		bestPairIndices.append(b2Index)
+
+		self.breedPair(bestPairIndices)
+		print("Best solution so far: " + str(self.population[0]))
+
+	def breedPair(self,parentIndices):
 		mix1 = self.population[parentIndices[0]]
 		mix2 = self.population[parentIndices[1]]
 
-		#mix by taking front and end of each and swapping.
-		mid = len(mix1) / 2
+		#generate a random mixpoint to attempt crossover from.
+		mid = random.randint(1,len(mix1) - 2)
 
+		#if the mix is small, just use the midpoint each time.
+		if(mid == 0 or mid == len(mix1)-1):
+			mix = len(mix1) / 2
 
-
-
+	#mix by taking front and end of each and swapping.
 		child1 = mix1[0:mid]
 		child1.extend(mix2[mid:]) #might need to be mid + 1
 		child2 = mix2[0:mid]
 		child2.extend(mix1[mid:])
 
-		if(random.randint(0,100) < 1001):
+		#mutate
+		if(random.randint(0,100) < 7):
 			i1 = random.randint(0,len(child1) - 1)
 			i2 = random.randint(0,len(child1) - 1)
 			child1[i1], child2[i2] = child2[i2], child1[i1]
 
-		self.population[destinationIndices[0]] = child1
-		self.population[destinationIndices[1]] = child2
-		print("Insert new pair at indices: " + str(destinationIndices[0]) + " and " + str(destinationIndices[1]))
+		
+		c1Fitness = self.fitnessFunction(child1)
+		c2Fitness = self.fitnessFunction(child2)
+		self.insertChildIntoPopulation(child1,c1Fitness)
+		self.insertChildIntoPopulation(child2,c2Fitness)
 
+	def insertChildIntoPopulation(self,child,fitness):
+		ptr = 0
+		while(fitness > self.fitness[ptr] and ptr < len(self.population)):
+			ptr += 1
+		self.population.insert(ptr,child)
+		self.fitness.insert(ptr,fitness)
+		print(self.fitness) 
 	def getBestMix(self):
-		return self.population[self.bestIndex]
+		return self.population[0]
 	def getTwoMinFitnessIndices(self,fitness):
 		firstMinIndex = 0
-		secondMinIndex = -1
+		secondMinIndex = 1
 		i = 1
 		while(i < len(fitness)):
 			if(fitness[i] < fitness[firstMinIndex]):
